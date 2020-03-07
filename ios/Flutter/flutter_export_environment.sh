a2377fc7f471b5b6ed7346cdf9db60ac9b820994
vi.vu001 <viet.vu001@stud.fh-dortmund.de>
2020-03-07 21:03:56 +0100

    generated a map of questions with results, show score when choosing answer

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=D:\Setup\Lernen\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Projects\flutter.projects\quizzler_project"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\Setup\Lernen\flutter\bin\cache\artifacts\engine\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
---
+++ lib/main.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Quizzler());
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scores = [];
+
+  Map<String, bool> questions = {
+    'You can lead a cow down stairs but not up stairs.': false,
+    'Approximately one quarter of human bones are in the feet.': true,
+    'A slug\'s blood is green.': true
+  };
+
+  int currentPos = 0;
+
+  void _increaseQuestionsNr() {
+    if (currentPos < questions.length - 1) {
+      setState(() {
+        currentPos++;
+      });
+    }
+  }
+
+  Widget _showQuestion() {
+    String question = questions.keys.elementAt(currentPos);
+    return Expanded(
+      flex: 5,
+      child: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Center(
+          child: Text(
+            question,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _addIcon() {
+    if (currentPos != questions.length) {
+      bool result = questions.values.elementAt(currentPos);
+      if (result) {
+        scores.add(Icon(Icons.check, color: Colors.green));
+      } else {
+        scores.add(Icon(Icons.close, color: Colors.deepOrange));
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        _showQuestion(),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+                setState(() {
+                  _addIcon();
+                  _increaseQuestionsNr();
+                  _showQuestion();
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                setState(() {
+                  _addIcon();
+                  _increaseQuestionsNr();
+                  _showQuestion();
+                });
+              },
+            ),
+          ),
+        ),
+        Row(children: scores)
+      ],
+    );
+  }
+}
